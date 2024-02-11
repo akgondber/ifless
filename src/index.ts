0be3f7678076d46aa91e57710b4f700b9bc879a9
Rushan Alyautdinov <akgondber@gmail.com>
2024-02-11 20:21:04 +0300

    "feat: Export string utils"

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Ifless from './ifless.js';
 import IflessNumber from './ifless-number';
-import IflessString from './ifless-string';
+import IflessString, {isEnglishChar, isNumeric} from './ifless-string';
 import IflessObject from './ifless-object';
 
 export default Ifless;
 
 const ifless = new Ifless();
 
-export {ifless, IflessNumber, IflessString, IflessObject};
+export {ifless, IflessNumber, IflessString, IflessObject, isEnglishChar, isNumeric};
